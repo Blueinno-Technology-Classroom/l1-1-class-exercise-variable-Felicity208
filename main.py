5b00586a1c6994a28db576acb21786397b1ed956
Felicity <felicity.curreem@gmail.com>
2025-01-11 11:44:15 +0800

    L1 (2)

--- main.py
+++ main.py
@@ -45,7 +45,7 @@ first += second
 second -= first
 first -= -second
 
-print('Q3:')
+print('Q4:')
 print(f'first = {first}')
 print(f'second = {-second}')
 print('#' * 30)
@@ -58,43 +58,48 @@ Q5:
 '''
 
 # : Write your code here
+print("Q5")
 work_hours = 4 + 5 + 8 + 4
 salary = 8.75
+tax = 0.2
+
 print("My total hours worked:")
 print(work_hours)
 # 
 print("My hourly salary:")
-print(salary)
+print(f'${salary}')
 #
 print("My total pay:")
 print(f'{(work_hours) * salary}')
-my_total_pay = work_hours * salary
 #
 print("My taxes owed:")
-print(my_total_pay * 0.2)
+print(work_hours * salary * tax)
 # 
+print("#" * 30)
 
 
 ##################################################
 '''
 Q6:
 '''
-
+print("Q6:")
 subtotal = 38 + 40 + 30
-tax = 0.08
-tip = 0.15
+tax_rate = 0.08
+tip_rate = 0.15
 # 
 print("Subtotal:")
 print(f'{subtotal}')
 # 
 print("Tax:")
-print(subtotal * tax)
+print(subtotal * tax_rate)
 # 
 print("Tip:")
-print(subtotal * tip)
+print(subtotal * tip_rate)
 # 
 print("Total:")
-print(subtotal + subtotal * tax + subtotal * tip)
+print(subtotal * (1 + tax_rate + tip_rate))
+
+print('#' * 30)
 #: Write your code here
 
 ##################################################
